bdb4c2182be1ed59a5963c7461355d0e844c23a6
kd Bromley <kd.bromley1@gmail.com>
2021-01-10 16:34:42 -0800

    NoteList component functional

--- src/NoteList/NoteList.js
+++ src/NoteList/NoteList.js
@@ -1,12 +1,28 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
 import NoteItem from '../NoteItem/NoteItem';
-import './NoteList.css'
+import './NoteList.css';
 
 export default function NoteList(props) {
     return (
-        <ul className='FolderList'>
-        {props.store["notes"].map(note =>
-            <NoteItem key={note.id} note={note}/> 
-        )}
-       </ul>
+        <section className='NoteList'>
+         <ul>
+            {props.store["notes"].map(note =>
+                <NoteItem key={note.id} note={note}/> 
+            )}
+         </ul>
+
+         <div className='NoteList__button'>
+            <button type='button'
+             className='NoteList__button-add'
+             disabled>
+                Add Note
+            </button>
+         </div>
+        </section>    
     )
+}
+
+NoteList.defaultProps = {
+    notes: []
 }
