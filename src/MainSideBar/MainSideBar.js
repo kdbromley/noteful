6ff0d3cfa4369058402fe4537ea1e7830fbaf45c
kd Bromley <kd.bromley1@gmail.com>
2021-01-10 16:26:21 -0800

    MainSideBar component functional

--- src/MainSideBar/MainSideBar.js
+++ src/MainSideBar/MainSideBar.js
@@ -1,9 +1,34 @@
-import NoteItem from "../NoteItem/NoteItem"
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import './FolderList.css';
 
 
-export default function FolderList(props) {
+export default function MainSideBar(props) {
     return (
-        <ul className='FolderList'>
-        </ul>
+        <div className='MainSideBar'>
+            <ul className='MainSideBar__list'>
+                {props.folders.map(folder =>
+                    <li key={folder.id}>
+                        <NavLink 
+                          classname='MainSideBar__folder-link'
+                          to={`/folder/${folder.id}`}
+                        >
+                            {folder.name}
+                        </NavLink>
+                    </li>
+                )}
+            </ul>
+            <div className='MainSideBar__button'>
+                <button type='button'
+                 className='MainSideBar__button-add' 
+                 disabled>
+                    Add Folder
+                </button>
+            </div>
+        </div>
     )
+}
+
+NoteListNav.defaultProps = {
+    folders: []
 }
