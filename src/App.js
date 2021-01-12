b7ec812d5195750d2d0dc44b7ce78ea29633d462
kd Bromley <kd.bromley1@gmail.com>
2021-01-11 16:15:33 -0800

    add component folders

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
-import NoteItem from './NoteItem/NoteItem';
+import dummy from './dummy-store';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Noteful</h1>
-      </header>
-      <main>
-        <NoteItem />
-      </main>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Noteful</h1>
+        </header>
+        <main>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
