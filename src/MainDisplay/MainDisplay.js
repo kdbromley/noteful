510fffe94f605deec9dd08505ce69e2a7e5745ee
kd Bromley <kd.bromley1@gmail.com>
2021-01-11 16:22:32 -0800

    rename MainSidebar to Sidebar

---
+++ src/MainDisplay/MainDisplay.js
@@ -0,0 +1,8 @@
+export default function MainDisplay() {
+    return (
+        <div className='MainDisplay'>
+            <p>Here are all your notes!
+            </p>
+        </div>
+    )
+}
---
+++ src/Sidebar/SideBar.js
@@ -0,0 +1,12 @@
+import './Sidebar.css';
+
+export default function MainSidebar() {
+    return (
+        <div className='MainSidebar'>
+            <ul>
+                <li>Folder 1</li>
+                <li>Folder 2</li>
+            </ul>
+        </div>
+    )
+}
