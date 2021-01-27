82b6757de385e8bd9b3607c37fabb1f575009bbf
kd Bromley <kd.bromley1@gmail.com>
2021-01-27 08:12:52 -0800

    notepagesidebar restore

--- src/NotePageSideBar/NotePageSideBar.js
+++ src/NotePageSideBar/NotePageSideBar.js
@@ -7,12 +7,43 @@ import './NotePageSidebar.css';
 
 
 export default class NotePageSideBar extends Component {
+    static contextType = NotesContext;
+    static defaultProps= {
+        history: {
+            goBack: () => { }
+        },
+        match: {
+            params: {}
+        }
+    }
 
     render() {  
+        const { notes=[], folders=[] } =  this.context
+        const { noteId } = this.props.match.params
+        const note = findNote(notes, noteId) || {}
+        const folder = findFolder(folders, note.folderId) || {}
         return (
-            <div className='Sidebar'>
-                <h3>{folder.name}</h3>
+            <div key={folder.id} className='Sidebar'>
+                <button type='button' className='Sidebar__back-button'>
+                <Link to='/'>Back</Link>
+                </button>
+                {folder && (
+                    <h3 className='Sidebar__folder-name'>
+                     <span>&#12304;</span> {folder.name} <span>&#12305;</span>
+                    </h3>
+                )}
             </div>
         )
     }
+}
+
+NotePageSideBar.propTypes = { 
+    history: PropTypes.object,
+    location: PropTypes.object,
+    match: PropTypes.shape({
+        isExact: PropTypes.bool,
+        params: PropTypes.object,
+        path: PropTypes.string,
+        url: PropTypes.string
+    })
 }
