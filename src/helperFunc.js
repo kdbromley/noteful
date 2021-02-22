6ca3fa4016fd871853880ab71cfbf38d1abcd3bf
kd Bromley <kd.bromley1@gmail.com>
2021-02-21 18:28:05 -0800

    cleanup unused code

--- src/helperFunc.js
+++ src/helperFunc.js
@@ -15,11 +15,3 @@ export const getNotes = (notes=[], folderId) => {
 export const matchFolderName = (folders=[], folderName) => {
   return folders.find(folder => folderName === folder.folder_name)
 }
-
-export const randomString = (length, chars) => {
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-    return result;
-}
-
-export const date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
