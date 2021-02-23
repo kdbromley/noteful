522961dba171e8cca13a6de890c607475d93926f
kd Bromley <kd.bromley1@gmail.com>
2021-02-23 09:48:19 -0800

    connect to deployed server/database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    NOTES_ENDPOINT: `http://localhost:8000/api/notes`,
-    FOLDERS_ENDPOINT: `http://localhost:8000/api/folders`
+    NOTES_ENDPOINT: `https://stark-sea-53677.herokuapp.com/api/notes`,
+    FOLDERS_ENDPOINT: `https://stark-sea-53677.herokuapp.com/api/folders`
 }
 
 export default config;
